dc0cdd4a2c2e09673ad7228cc0e414f4755ed283
Sabelonjabulo <sabelo.n.mashinini@gmail.com>
2023-03-16 06:24:33 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+
+*main - Entry point
+*Description: This program prints a string using the puts function.
+*Return: 0
+*/
+
 int main() {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
