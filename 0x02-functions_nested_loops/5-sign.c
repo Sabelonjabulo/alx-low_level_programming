08bc7fa90c57ef0d2c173c300494cb1c8530bfdf
Sabelonjabulo <sabelo.n.mashinini@gmail.com>
2023-03-21 06:32:09 -0700

    sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - prints the sign of a number
  * @n: the number to be checked
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, 
+ * Return: 1 if n is greater than zero, 0 if n is zero,
  * -1 if n is less than zero
  */
 int print_sign(int n)
