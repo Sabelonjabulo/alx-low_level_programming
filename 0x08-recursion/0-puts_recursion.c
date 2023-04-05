0897f2ed4463da82d6ed9de2078b87b113ca4a81
Sabelonjabulo <sabelo.n.mashinini@gmail.com>
2023-04-05 08:29:33 -0700

    task 0 and task 1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  * Return: Always 0 (Success)
  */
 
-void _puts_recursion(char*s)
+void _puts_recursion(char *s)
 {
 	if(*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
